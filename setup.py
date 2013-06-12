48510eeb9bef53e0b62838f2cb9d794848d96775
Mark Steve Samson <hello@marksteve.com>
2013-06-12 22:51:08 +0800

    Fix setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ setup(
   name='Flask-Captain',
   description="Handle webhooks with Flask",
   version='0.0.1',
-  author_name='Mark Steve Samson',
+  author='Mark Steve Samson',
   author_email='hello@marksteve.com',
+  url='https://github.com/marksteve/flask-captain',
   py_modules=['flask_captain'],
   install_requires=[
     'Flask',
